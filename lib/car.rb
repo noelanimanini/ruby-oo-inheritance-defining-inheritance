c0fc35659c9de30e066904bd5cfd18d08a629bc3
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-11-24 13:39:21 -0800

    supa class

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+end 
+
+class Car <Vehicle
+    def go 
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end 
+end 
+
+#Car class inherit all of the Vehicle
